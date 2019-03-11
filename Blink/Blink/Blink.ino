53ea29219e5eece0458cd0033c7fc839ec1f88a0
borxoloko <borxoloko@live.com>
2019-03-11 01:22:55 +0100

    Blink

---
+++ Blink/Blink/Blink.ino
@@ -0,0 +1,20 @@
+int led = 13;
+int led1 = 12;
+
+void setup()
+	{
+		pinMode(led, OUTPUT);
+		pinMode(led1, OUTPUT);
+	}
+
+
+void loop()
+	{
+		digitalWrite(led, HIGH);
+		digitalWrite(led1, LOW);
+		delay(1000);
+
+		digitalWrite(led, LOW);
+		digitalWrite(led1, HIGH);
+		delay(1000);
+	}
