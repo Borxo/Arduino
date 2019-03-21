6305c459939565d111fa3c2e59934c25d2dc4ca2
Borxoloko <borxoloko@live.com>
2019-03-21 20:50:09 +0100

    Update PWM y ADC.ino
    Modificion_Formula

--- PWM y ADC/PWM y ADC/PWM y ADC.ino
+++ PWM y ADC/PWM y ADC/PWM y ADC.ino
@@ -1,40 +1,44 @@
+
 #define Led 5
 #define Led1 4 
 #define enable 3
 #define MotorDerecha 9
 #define MotorIzquierda 10 
-
+#define enable 3
 
 
 void setup()
-	{
-		Serial.begin(9600);
-		pinMode(Led, OUTPUT);
-		pinMode(Led1, OUTPUT);
-		pinMode(MotorDerecha, OUTPUT);
-		pinMode(MotorIzquierda, OUTPUT);
+  {
+    Serial.begin(9600);
+    pinMode(Led, OUTPUT);
+    pinMode(Led1, OUTPUT);
+    pinMode(MotorDerecha, OUTPUT);
+    pinMode(MotorIzquierda, OUTPUT);
 
-	}
-	
+  }
+  
 void loop()
-	{
-		digitalWrite(MotorDerecha, HIGH);
-		digitalWrite(MotorIzquierda, LOW);
-		int Velocidad = analogRead(A0) / 10;
-		analogWrite(enable, Velocidad*2.55);
+  {
+     digitalWrite(MotorDerecha, HIGH);
+     digitalWrite(MotorIzquierda, LOW);
+    float Velocidad = (5.0/1024)*analogRead(A0);
+    float Control=(1/(0.072*Velocidad+0.006))-0.42;
+    analogWrite(enable, Control);
 
-		Serial.print(Velocidad);
-		Serial.println("c");
-		delay(100);
+    Serial.print(Control);
+    Serial.print("cm     ");
+    Serial.print(Velocidad);
+    Serial.println("V");
+    delay(1000);
 
-		if (Velocidad > 0)
-			{
-				digitalWrite(Led, HIGH);
-				digitalWrite(Led1, LOW);
-			}
-		else if (Velocidad == 0)
-			{
-				digitalWrite(Led1, HIGH);
-				digitalWrite(Led, LOW);
-			}
-	}
+    if (Velocidad > 0)
+      {
+        digitalWrite(Led, HIGH);
+        digitalWrite(Led1, LOW);
+      }
+    else if (Velocidad == 0)
+      {
+        digitalWrite(Led1, HIGH);
+        digitalWrite(Led, LOW);
+      }
+  }
