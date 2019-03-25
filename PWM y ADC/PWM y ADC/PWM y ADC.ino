641ca56336b9c94cd1faa3ea8f1caa3cc40b0088
borxoloko <borxoloko@live.com>
2019-03-25 10:03:13 +0100

    PWM y ADC

--- PWM y ADC/PWM y ADC/PWM y ADC.ino
+++ PWM y ADC/PWM y ADC/PWM y ADC.ino
@@ -42,13 +42,13 @@ void loop()
     delay(1000);
 
 
-    if (Val >= 8)
+    if (Val >= 10)
       {
         digitalWrite(Led, HIGH);
         digitalWrite(Led1, LOW);
       }
 
-    else if (Val <= 5)
+    else if (Val < 10)
       {
         digitalWrite(Led1, HIGH);
         digitalWrite(Led, LOW);
